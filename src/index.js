143961401bfb79202f1595821ee61d7e12a77f3b
surtich <surtich@gmail.com>
2021-11-10 12:50:15 +0100

    [webpack] Using css loader

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import lodash from 'lodash';
+import './style.css';
 
 function component() {
     const element = document.createElement('div');
 
     element.innerHTML = lodash.join(['Hello', 'webpack'], ' ');
+    element.classList.add('hello');
 
     return element;
 }
