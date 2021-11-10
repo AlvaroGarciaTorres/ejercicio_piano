8330262b4937198273dff979a6b4ac41daec6caf
surtich <surtich@gmail.com>
2021-11-10 12:58:26 +0100

    [webpack] Using multiple bundlers

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 import lodash from 'lodash';
 import './style.css';
+import printMe from './print.js';
 
 function component() {
     const element = document.createElement('div');
 
     element.innerHTML = lodash.join(['Hello', 'webpack'], ' ');
     element.classList.add('hello');
+
+    const btn = document.createElement('button');
+    btn.innerHTML = 'Click me and check the console!';
+    btn.onclick = printMe;
+
+    element.appendChild(btn);
 
     return element;
 }
